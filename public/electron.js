c9525b7e39ca65f59d219869fb062f95967274bb
김지훈 <kim@jihun.io>
2024-11-25 12:45:00 +0900

    Electron 설치 및 설정

---
+++ public/electron.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : `file://${__dirname}/index.html`;
+  win.loadURL(startUrl);
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
