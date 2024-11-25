41907ba07f1d0d5fc7ab731523807583fcf38448
김지훈 <kim@jihun.io>
2024-11-25 16:58:58 +0900

    가상 요소 관련 pattern 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,12 +16,21 @@ module.exports = {
     "flex-row",
     "justify-center",
     "items-center",
+    // pseudo-elements 관련 클래스 추가
+    {
+      pattern: /before:(content-\[.*\]|block|opacity-0)/,
+    },
     {
       pattern: /font-(thin|light|normal|medium|bold|black)/,
+    },
+    {
       pattern: /text-(xs|sm|base|lg|xl|2xl|3xl)/,
+    },
+    {
       pattern: /text-gray-(50|100|200|300|400|500|600|700|800|900)/,
+    },
+    {
       pattern: /gap-(0|1|2|3|4|5|6|8|10|12|16)/,
-      variants: ["hover", "focus", "lg", "dark"],
     },
   ],
   plugins: [require("@tailwindcss/typography")],
