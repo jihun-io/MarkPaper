4011d1b57551ab046a379029e59027aa6d14e1e8
김지훈 <kim@jihun.io>
2024-11-26 16:30:01 +0900

    헤딩 태그의 크기를 상대 크기로 설정

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -13,9 +13,9 @@ export default function Preview({ previewRef, paperWidth, paperHeight, html }) {
       <div
         className={`h-full max-h-full print:m-0 text-[12pt] prose max-w-none
                 prose-headings:font-bold
-                prose-h1:text-2xl prose-h1:my-4 prose-h1:leading-[1.3]
-                prose-h2:text-xl prose-h2:mt-4 prose-h2:mb-0
-                prose-h3:text-lg prose-h3:mt-4 prose-h3:mb-4
+                prose-h1:text-[1.5em] prose-h1:my-4 prose-h1:leading-[1.3]
+                prose-h2:text-[1.25em] prose-h2:mt-4 prose-h2:mb-0
+                prose-h3:text-[1.125em] prose-h3:mt-4 prose-h3:mb-4
                 prose-p:leading-[1.7] prose-p:my-2
                 prose-a:no-underline hover:prose-a:underline
                 prose-blockquote:border-l-4 
