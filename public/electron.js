29a5b806ea86efd88349bdb62c80252e3457ddee
김지훈 <kim@jihun.io>
2024-11-27 01:47:50 +0900

    저장 대화 상자 텍스트 수정

--- public/electron.js
+++ public/electron.js
@@ -248,7 +248,7 @@ ipcMain.handle("show-close-confirmation", async (event) => {
 
   const { response } = await dialog.showMessageBox(win, {
     type: "question",
-    buttons: ["저장 후 종료", "저장하지 않고 종료", "취소"],
+    buttons: ["저장 후 닫기", "저장하지 않고 닫기", "취소"],
     defaultId: 0,
     title: "저장되지 않은 변경사항",
     message: "저장되지 않은 변경사항이 있습니다. 저장하시겠습니까?",
