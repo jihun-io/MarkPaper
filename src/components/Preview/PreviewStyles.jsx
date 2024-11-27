1131e79d18676ab260d176bb8ddc43c057ba549a
김지훈 <kim@jihun.io>
2024-11-27 10:33:18 +0900

    코드 블록 스타일 수정

--- src/components/Preview/PreviewStyles.jsx
+++ src/components/Preview/PreviewStyles.jsx
@@ -25,13 +25,16 @@ export default function PreviewStyles({
                 prose-a:no-underline hover:prose-a:underline
                 prose-blockquote:border-l-4 
                 prose-blockquote:pl-4 prose-blockquote:py-1 
-                prose-ul:list-disc prose-ul:pl-4 prose-ul:mt-0 prose-ul:mb-2
+                prose-li:marker:text-black
                 prose-li:my-[0.2rem]
                 prose-strong:font-[700]
                 prose-table:w-fit prose-table:mt-4 prose-table:mb-0 prose-table:text-[12pt]
                 prose-thead:border-none
                 prose-th:py-0
                 prose-td:py-0
+                prose-pre:!bg-gray-100
+                prose-code:!text-black
+                prose-code:before:content-[''] prose-code:after:content-['']
                 after:content-[''] after:block after:opacity-0 after:w-full after:h-[100vh] after:print:hidden
                 `}
         dangerouslySetInnerHTML={{ __html: html }}
